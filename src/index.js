68164e09fe1c48cb94df85abd054063ab6635fc8
Sergei_L <lubeshkosergei@gmail.com>
2023-01-23 22:57:04 +0300

    feat:solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let union_bracket = bracketsConfig.map((item) => {
+        return `${item[0]}${item[1]}`;
+    });
+
+    let str_len_beg = str.length;
+
+    while (str.length) {
+        union_bracket.forEach((item) => {
+            str = str.replace(item, "");
+        });
+
+        if (str.length === str_len_beg) {
+            return false;
+        }
+
+        str_len_beg = str.length;
+    }
+
+    return true;
 }
